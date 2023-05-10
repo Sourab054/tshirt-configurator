65aacd0109bed09ad43416630ad035240a0650d3
Sourab Patil <sourab.patil@wowlabz.comgit config --global user.name Sourab>
2023-05-10 10:19:29 +0530

    Fixed AI generator

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -6,14 +6,15 @@ import { fadeAnimation, slideAnimation } from "../config/motion";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import Tab from "../components/Tab";
 import { AIPicker, ColorPicker, CustomButton, FilePicker } from "../components";
+import { reader } from "../config/helpers";
 
 const Customizer = () => {
   const snap = useSnapshot(state);
 
   const [file, setFile] = useState("");
 
-  const [prompt, setPrompt] = useState("");
-  const [generatingImg, setGeneratingImg] = useState(false);
+  // const [prompt, setPrompt] = useState("");
+  // const [generatingImg, setGeneratingImg] = useState(false);
 
   const [activeEditorTab, setActiveEditorTab] = useState("");
   const [activeFilterTab, setActiveFilterTab] = useState({
@@ -21,28 +22,90 @@ const Customizer = () => {
     stylishShirt: false,
   });
 
+  // const handleSubmit = async (type) => {
+  //   if (!prompt) return alert("Please enter a prompt");
+
+  //   try {
+  //     setGeneratingImg(true);
+
+  //     const response = await fetch("http://localhost:8080/api/v1/dalle", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         prompt,
+  //       }),
+  //     });
+
+  //     const data = await response.json();
+
+  //     handleDecals(type, `data:image/png;base64,${data.photo}`);
+  //   } catch (error) {
+  //     alert(error);
+  //   } finally {
+  //     setGeneratingImg(false);
+  //     setActiveEditorTab("");
+  //   }
+  // };
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  };
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+        state.isFullTexture = !activeFilterTab[tabName];
+        break;
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
+    }
+
+    // after setting the state, activeFilterTab is updated
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName],
+      };
+    });
+  };
+
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    });
+  };
+
   // show tab content depending on the activeTab
   const generateTabContent = () => {
     switch (activeEditorTab) {
       case "colorpicker":
         return <ColorPicker />;
       case "filepicker":
-        return (
-          <FilePicker
-          // file={file}
-          // setFile={setFile}
-          // readFile={readFile}
-          />
-        );
-      case "aipicker":
-        return (
-          <AIPicker
-          // prompt={prompt}
-          // setPrompt={setPrompt}
-          // generatingImg={generatingImg}
-          // handleSubmit={handleSubmit}
-          />
-        );
+        return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
+      // case "aipicker":
+      //   return (
+      //     <AIPicker
+      //       prompt={prompt}
+      //       setPrompt={setPrompt}
+      //       generatingImg={generatingImg}
+      //       handleSubmit={handleSubmit}
+      //     />
+      //   );
       default:
         return null;
     }
@@ -89,7 +152,13 @@ const Customizer = () => {
             {...slideAnimation("up")}
           >
             {FilterTabs.map((tab) => (
-              <Tab key={tab.name} tab={tab} isFilterTab isActiveTab="" />
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
+              />
             ))}
           </motion.div>
         </>
